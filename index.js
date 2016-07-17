0f120d40e38e62fd3d473ab5554d0bfcfd811fe6
James Hennessy <james.ryan.hennessy@gmail.com>
2016-07-16 22:12:22 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,46 +1,153 @@
-'use strict'
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
 
-app.set('port', (process.env.PORT || 5000))
+   
+    var express = require('express')
+    var bodyParser = require('body-parser')
+    var request = require('request')
+    var app = express()
 
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+    app.set('port', (process.env.PORT || 5000))
 
-// Process application/json
-app.use(bodyParser.json())
+    // parse application/x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({extended: false}))
 
-// Index route
-app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
-})
+    // parse application/json
+    app.use(bodyParser.json())
 
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-        res.send(req.query['hub.challenge'])
+    // index
+    app.get('/', function (req, res) {
+	    res.send('hello world i am a secret bot')
+	})
+    //add a hashtable to easy communciate
+    // for facebook verification
+    app.get('/webhook/', function (req, res) {
+	    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+		res.send(req.query['hub.challenge'])
+	    }
+	    res.send('Error, wrong token')
+	})
+
+    // to post data
+    app.post('/webhook/', function (req, res) {
+	        messaging_events = req.body.entry[0].messaging
+		for (i = 0; i < messaging_events.length; i++) {
+		    event = req.body.entry[0].messaging[i]
+		    sender = event.sender.id
+		    if (event.message && event.message.text) {
+			    text = event.message.text
+			    if (text === 'Generic') {
+				sendGenericMessage(sender)
+				continue
+			    }
+			    else if(text == 'whats up'){
+				sendHiMessage(sender)
+				continue 
+			    }
+			    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+		    }
+		    if (event.postback) {
+			text = JSON.stringify(event.postback)
+			sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
+			    continue
+		    }
+		}
+		res.sendStatus(200)
+	})
+
+var token = "EAAMeZARHIFrMBAFUtJ6QMa7uf6ZCmKa4nRVMxaV7ZC8PBnWIovAotP8iZCfGUCTIzSwKgyvSMY4AqZATpjdnsDE8xH5L1Xeoep5EMVZBflDegNtsE59f0xG5KMG2nC6pZCAUYW5G8GEj44p9InCvdm7bdNbl5fP1UQjG6jubJMBrAZDZD"
+
+
+    function sendTextMessage(sender, text) {
+    messageData = {
+	text:text
     }
-    res.send('Error, wrong token')
-})
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:token},
+	    method: 'POST',
+	    json: {
+		recipient: {id:sender},
+		message: messageData,
+	    }
+	}, function(error, response, body) {
+	    if (error) {
+		console.log('Error sending messages: ', error)
+	    } else if (response.body.error) {
+		console.log('Error: ', response.body.error)
+	    }
+	})
+}
+    function sendHiMessage(sender){
+	messageData={
+	    text:"fuck you"
+	}
+	request({
+		url: 'https://graph.facebook.com/v2.6/me/messages',
+		    qs: {access_token:token},
+		    method: 'POST',
+		    json: {
+		    recipient: {id:sender},
+			message: messageData,
+			}
+	    }, function(error, response, body) {
+		if (error) {
+		    console.log('Error sending messages: ', error)
+			} else if (response.body.error) {
+		    console.log('Error: ', response.body.error)
+			}
+	    })
 
-// Spin up the server
+	    }
+function sendGenericMessage(sender) {
+    messageData = {
+	"attachment": {
+	    "type": "template",
+	    "payload": {
+		"template_type": "generic",
+		"elements": [{
+			"title": "First card",
+			"subtitle": "Element #1 of an hscroll",
+			"image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+			"buttons": [{
+				"type": "web_url",
+				"url": "https://www.messenger.com",
+				"title": "web url"
+			    }, {
+				"type": "postback",
+				"title": "Postback",
+				"payload": "Payload for first element in a generic bubble",
+			    }],
+		    }, {
+			"title": "Second card",
+			"subtitle": "Element #2 of an hscroll",
+			"image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+			"buttons": [{
+				"type": "postback",
+				"title": "Postback",
+				"payload": "Payload for second element in a generic bubble",
+			    }],
+		    }]
+	    }
+	}
+    }
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+		qs: {access_token:token},
+		method: 'POST',
+		json: {
+		recipient: {id:sender},
+		    message: messageData,
+		    }
+	}, function(error, response, body) {
+	    if (error) {
+		console.log('Error sending messages: ', error)
+		    } else if (response.body.error) {
+		console.log('Error: ', response.body.error)
+		    }
+	})
+	}
+
+// spin spin sugar
 app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-})
-app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
-    for (let i = 0; i < messaging_events.length; i++) {
-        let event = req.body.entry[0].messaging[i]
-        let sender = event.sender.id
-        if (event.message && event.message.text) {
-            let text = event.message.text
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-        }
-    }
-    res.sendStatus(200)
-})
-
-const token = "EAAMeZARHIFrMBADDrhcDKDsrIqfy18o4eZBnNZAjkUUGEywrmCgFGJeSEZCb5vxwH7YUFc2EyzRJ0fczONWZBUZBP2huJ1J20kHw5LaZB6QMmbPkWO4LqUZBmoMONjqd35aZC0yyZA38JDP1MXJmPULy6WZA2MictfIjgW4LDVzju2rNgZDZD"
+	console.log('running on port', app.get('port'))
+	    })
